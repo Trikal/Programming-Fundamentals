b073745d5d4549d71d8e7aa144fb9c38d1c7ede9
Trikal <thapamanis66@gmail.com>
2016-10-21 11:38:37 +0545

    Add files via upload

---
+++ leepyear.c
@@ -0,0 +1,30 @@
+/*
+student name:Manish Thapa 
+subject:programing fundamental
+roll:
+lab no:3
+programe :
+date:
+*/
+   #include<stdio.h>
+  
+     int main()
+           {
+          int days;
+         printf("enter the days of feburary");
+          scanf("%d",&days);
+        
+               
+                if(days==29){
+                   printf("feburary is  a leep year");
+                    }
+              
+                   else{
+                   printf("feburary is a not leep year");
+                    }
+
+
+return 0;
+}
+
+
---
+++ thapakazi.c
@@ -0,0 +1,32 @@
+/*
+student name:Manish Thapa 
+subject:programing fundamental
+roll:
+lab no:3
+programe :
+date:
+*/
+   #include<stdio.h>
+  
+     int main()
+              {
+              int a,b;
+              printf("enter the first number");
+              scanf("%d",&a);
+              printf("enter the second number");
+              scanf("%d",&b);
+
+                printf("Use of AND(&) Operator:%d\n",a&b);
+		printf("Use of OR(|) Operator:%d\n",a|b);
+		printf("Use of complement(~) Operator:%d\n",~a);
+		printf("Use of X-OR(^) Operator:%d\n",a^b);
+              
+              return 0;
+
+
+
+              }
+
+
+
+

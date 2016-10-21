a5058b9f0bc4730e142809f4cb3ec1562f10aee0
Trikal <thapamanis66@gmail.com>
2016-10-21 11:38:07 +0545

    Add files via upload

---
+++ else.c
@@ -0,0 +1,40 @@
+/*
+student name:Manish Thapa 
+subject:programing fundamental
+roll:
+lab no:4
+programe :
+date:
+*/
+   #include<stdio.h>
+  
+     int main()
+           {
+ 
+      int a,b,c;
+       printf("enter the first number:\n");
+        scanf("%d",&a);
+             
+                  printf("enter the second number:\n");
+                scanf("%d",&b);
+
+             printf("enter the third number:\n");
+                scanf("%d",&c);
+
+              if(a>=b && a>=c)
+                printf("a is the greater\n");
+
+              else if(b>=c && b>=a)
+                printf("b is the greater\n");
+
+
+                  
+              else 
+                printf("c is the greater\n");
+            return 0;
+}
+
+
+
+
+
---
+++ ifelse.c
@@ -0,0 +1,37 @@
+/*
+student name:Manish Thapa 
+subject:programing fundamental
+roll:
+lab no:4
+programe :
+date:
+*/
+   #include<stdio.h>
+  
+     int main()
+           {
+ 
+      int a;
+       printf("enter the first number:/n");
+        scanf("%d",&a);
+
+          if(a%2==0)
+
+                {
+             printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+          return 0;
+}
+
+   
+   
+    
+         
+          
+         
+             
+
